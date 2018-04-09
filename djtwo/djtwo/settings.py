f41414180b3c7ea9f598ce91c4163dac2fc830b3
luohua13950 <luohua139@qq.com>
2018-04-09 16:30:35 +0800

    正常显示页面，并且有返回

--- djtwo/djtwo/settings.py
+++ djtwo/djtwo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djtwo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR,  'templates')],
+        'DIRS': [ os.path.join(BASE_DIR, 'learn/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- djtwo/learn/views.py
+++ djtwo/learn/views.py
@@ -4,18 +4,21 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,render_to_response
 from django import forms
 from learn.models import User
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 class UserForm(forms.Form):
     username = forms.CharField(label='用户名:',max_length=100)
     password = forms.CharField(label='密码:',widget=forms.PasswordInput())
 
 def login(request):
-    if request.method == 'post':
+    if request.method == "POST":
         uf = UserForm(request.POST)
         if uf.is_valid():
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
             user = User.objects.filter(username = username,password = password)
+            print username
             if user:
                 passwd = User.objects.filter(username = username,password = password)
                 if passwd:
@@ -23,14 +26,15 @@ def login(request):
                 else:
                     info = '登录失败，请检查用户名和密码！'
             elif len(user) == 0:
-                info = '检查是否输入'
+                info = '输入是否正确？'
             return HttpResponse(info)
-        else:
-            uf = UserForm()
-    return render_to_response('login.html',{'uf':uf})
-
+    else:
+        uf = UserForm()
+    #return render_to_response('login.html',{'uf':uf},RequestContext(request))
+    return render(request,'login.html',{'uf':uf})
 def index(request):
     return render(request,'login.html')
 
 def regist(request):
-    return render(request,'regist.html')
+    c = ({'name':'luohua'})
+    return render(request,'regist.html',c)
